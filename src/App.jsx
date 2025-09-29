5bf97aee970967786b58b0437096fe570206a629
Axel Closas <axelclosas13@gmail.com>
2025-09-29 10:41:56 -0300

    TP Opcional - Hola Mundo - Completo

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { HelloWorld } from './components/HelloWorld'
+
+function App() {
+  return (
+    <div className="w-full h-dvh grid place-content-center">
+      <HelloWorld />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,5 @@
+export function HelloWorld() {
+  return (
+    <h1 className="text-2xl bg-amber-100 p-4 rounded-lg border-l-4 border-l-amber-600 hover:bg-amber-200 hover:scale-110 transition">¡Hola, Mundo! 🌐</h1>
+  )
+}
